2a7f4fe1ce75493c8bf7169bf2a282d69ef7250b
Kauã Santos <116742943+Bladekkkkkk@users.noreply.github.com>
2024-03-18 18:34:20 -0400

    Meu primerio commit

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import Header from './componentes/Header'
+import Main from './componentes/Main'
+import Footer from './componentes/Footer'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Header/>
+    <Main/>
+    <Footer></Footer>
+  </React.StrictMode>,
+)
